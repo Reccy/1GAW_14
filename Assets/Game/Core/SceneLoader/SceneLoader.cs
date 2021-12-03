29972feab8453d0085b5b82ef7489876c1e7fc00
Aaron Meaney <aaronmeaney@hotmail.com>
2021-12-03 19:42:40 +0000

    Minor updates

--- Assets/Game/Core/SceneLoader/SceneLoader.cs
+++ Assets/Game/Core/SceneLoader/SceneLoader.cs
@@ -6,6 +6,12 @@ public class SceneLoader : MonoBehaviour
     [SerializeField]
     private int m_sceneIndex;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            LoadScene();
+    }
+
     public void LoadScene()
     {
         SceneManager.LoadScene(m_sceneIndex);
